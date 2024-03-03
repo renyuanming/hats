44d7072630215e77d44efcebc2ba6d267edd8bd9
yuanming <1985161994@qq.com>
2024-03-03 23:29:02 +0800

    debug DirectIOUtils.java

--- src/java/org/apache/cassandra/io/util/DirectIOUtils.java
+++ src/java/org/apache/cassandra/io/util/DirectIOUtils.java
@@ -103,14 +103,14 @@ public class DirectIOUtils
     */
     public static int read(FileChannel channel, ByteBuffer dst, long position) throws IOException
     {
-        int lim = dst.limit();
-        int r = (int) (position & (BLOCK_SIZE - 1));
-        int len = lim + r;
-        dst.limit((len & (BLOCK_SIZE - 1)) == 0 ? len : (len & -BLOCK_SIZE) + BLOCK_SIZE);
+        // int lim = dst.limit();
+        // int r = (int) (position & (BLOCK_SIZE - 1));
+        // int len = lim + r;
+        // dst.limit((len & (BLOCK_SIZE - 1)) == 0 ? len : (len & -BLOCK_SIZE) + BLOCK_SIZE);
         int n = channel.read(dst);
         logger.debug("rymDebug: read file : {}, position: {}, n: {}, lim: {}, r: {}, len: {}", channel.toString(),  position, n, lim, r, len);
         // n -= r;
-        n = n < lim ? n : lim;
+        // n = n < lim ? n : lim;
         // dst.position(r).limit(r + n);
         return n;
     }
