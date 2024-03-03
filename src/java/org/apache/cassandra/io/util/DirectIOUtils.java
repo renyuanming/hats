29b835efa10e910b2a29a5bdf2ac94502c3150a5
yuanming <1985161994@qq.com>
2024-03-03 23:30:58 +0800

    debug DirectIOUtils.java

--- src/java/org/apache/cassandra/io/util/DirectIOUtils.java
+++ src/java/org/apache/cassandra/io/util/DirectIOUtils.java
@@ -108,7 +108,7 @@ public class DirectIOUtils
         // int len = lim + r;
         // dst.limit((len & (BLOCK_SIZE - 1)) == 0 ? len : (len & -BLOCK_SIZE) + BLOCK_SIZE);
         int n = channel.read(dst);
-        logger.debug("rymDebug: read file : {}, position: {}, n: {}, lim: {}, r: {}, len: {}", channel.toString(),  position, n, lim, r, len);
+        // logger.debug("rymDebug: read file : {}, position: {}, n: {}, lim: {}, r: {}, len: {}", channel.toString(),  position, n, lim, r, len);
         // n -= r;
         // n = n < lim ? n : lim;
         // dst.position(r).limit(r + n);
