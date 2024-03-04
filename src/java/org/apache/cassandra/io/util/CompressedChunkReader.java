2719b31b394392f77c6ba397ca3879d730c4cecd
renyuanming <1985161994@qq.com>
2024-03-04 16:37:15 +0800

    debug CompressionMetadata.java

--- src/java/org/apache/cassandra/io/util/CompressedChunkReader.java
+++ src/java/org/apache/cassandra/io/util/CompressedChunkReader.java
@@ -146,9 +146,10 @@ public abstract class CompressedChunkReader extends AbstractReaderFileProxy impl
                         throw new CorruptBlockException(channel.filePath(), chunk);
                     }
 
-                    // if (metadata.chunksIndexFile.path().contains("7ad54392bcdd35a684174e047860b377")){
-                    //     AKUtils.printStackTace(AKLogLevels.INFO, String.format("rymINFO: the compressed.limit: %s, chunk.length is %s, length is %s, the read length is %s, channel is %s, the position of compressed chunk is %s, the fileLength is %s", compressed.limit(), chunk.length, length, readLength, channel.toString(), position, fileLength));
-                    // }
+                    if (metadata.chunksIndexFile.path().contains("7ad54392bcdd35a684174e047860b377")){
+                        logger.debug("rymDebug: the compressed.limit: {}, chunk.length is {}, length is {}, the read length is {}, channel is {}, the position of compressed chunk is {}, the fileLength is {}", compressed.limit(), chunk.length, length, readLength, channel.toString(), position, fileLength);
+                        // AKUtils.printStackTace(AKLogLevels.INFO, String.format("rymINFO: the compressed.limit: %s, chunk.length is %s, length is %s, the read length is %s, channel is %s, the position of compressed chunk is %s, the fileLength is %s", compressed.limit(), chunk.length, length, readLength, channel.toString(), position, fileLength));
+                    }
 
                     compressed.flip();
                     compressed.limit(chunk.length);
