9ab1430d29bbbbf45bbea3078e2a86bbe34de10a
yuanming <1985161994@qq.com>
2024-05-01 00:05:12 +0800

    change raft port

--- src/java/org/apache/cassandra/service/CassandraDaemon.java
+++ src/java/org/apache/cassandra/service/CassandraDaemon.java
@@ -431,7 +431,7 @@ public class CassandraDaemon
         // AdaptiveKV
         ElectionBootstrap.initElection(AKUtils.getRaftLogPath(), 
                                       "AdaptiveKV", 
-                                      DatabaseDescriptor.getListenAddress().getHostAddress()+":"+DatabaseDescriptor.getStoragePort(), 
+                                      DatabaseDescriptor.getListenAddress().getHostAddress()+":8081", 
                                       Gossiper.getSeedsStr());
 
         // schedule periodic background compaction task submission. this is simply a backstop against compactions stalling
