c67e9e7c3e95000250a61ee37a90d8088e2a3013
renyuanming <1985161994@qq.com>
2024-05-03 14:17:08 +0800

    update the debug log

--- src/java/com/alipay/sofa/jraft/core/Replicator.java
+++ src/java/com/alipay/sofa/jraft/core/Replicator.java
@@ -830,8 +830,8 @@ public class Replicator implements ThreadId.OnError {
 
                 addInflight(RequestType.AppendEntries, this.nextIndex, 0, 0, seq, rpcFuture);
             }
-            AKUtils.printStackTace(AKLogLevels.DEBUG, String.format("rymDebug: Node %s send HeartbeatRequest to %s term %s", this.options.getNode()
-            .getNodeId(), this.options.getPeerId(), this.options.getTerm()));
+            // AKUtils.printStackTace(AKLogLevels.DEBUG, String.format("rymDebug: Node %s send HeartbeatRequest to %s term %s", 
+            //                                                         this.options.getNode().getNodeId(), this.options.getPeerId(), this.options.getTerm()));
             LOG.debug("Node {} send HeartbeatRequest to {} term {} lastCommittedIndex {}", this.options.getNode()
                 .getNodeId(), this.options.getPeerId(), this.options.getTerm(), rb.getCommittedIndex());
         } finally {
