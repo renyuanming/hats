079b2896c2397bece10b9e2db5851d96ac84698f
renyuanming <1985161994@qq.com>
2024-06-03 12:46:06 +0800

    [scripts] update

--- scripts/run/start-client.sh
+++ scripts/run/start-client.sh
@@ -27,7 +27,7 @@ func() {
 
     cd $file_dir
 
-    if [ "$mode" == "mlsm" ]; then
+    if [ "$mode" == "mlsm" ] || [ "$mode" == "horse" ]; then
         echo "Enable multiple LSM tree"
         bin/cqlsh "$coordinator" -e "create keyspace ycsb WITH REPLICATION = {'class' : 'SimpleStrategy', 'replication_factor': $replication_factor };
         USE ycsb;
@@ -36,19 +36,12 @@ func() {
         ALTER TABLE usertable1 WITH compaction = { 'class': 'LeveledCompactionStrategy', 'sstable_size_in_mb': ${sstable_size}, 'fanout_size': ${fanout_size}};
         ALTER TABLE usertable2 WITH compaction = { 'class': 'LeveledCompactionStrategy', 'sstable_size_in_mb': ${sstable_size}, 'fanout_size': ${fanout_size}};
         consistency all;"
-    elif [ "$mode" == "cassandra" ]; then
-        echo "Disable multiple LSM-tree"
-        bin/cqlsh $coordinator -e "create keyspace ycsbraw WITH REPLICATION = {'class' : 'SimpleStrategy', 'replication_factor': $replication_factor };
-        USE ycsbraw;
-        create table usertable (y_id varchar primary key, field0 varchar);
-        ALTER TABLE usertable WITH compaction = { 'class': 'LeveledCompactionStrategy', 'sstable_size_in_mb': $sstable_size, 'fanout_size': $fanout_size};
-
-        consistency all;"
-    elif [ "$mode" == "depart" ]; then
+    elif [ "$mode" == "cassandra" ] || [ "$mode" == "depart" ]; then
         bin/cqlsh "$coordinator" -e "create keyspace ycsb WITH REPLICATION = {'class' : 'SimpleStrategy', 'replication_factor': $replication_factor };
         USE ycsb;
         create table usertable (y_id varchar primary key, field0 varchar);
-        ALTER TABLE usertable WITH compaction = { 'class': 'LeveledCompactionStrategy', 'sstable_size_in_mb': $sstable_size, 'fanout_size': $fanout_size}"
+        ALTER TABLE usertable WITH compaction = { 'class': 'LeveledCompactionStrategy', 'sstable_size_in_mb': $sstable_size, 'fanout_size': $fanout_size}
+        consistency all;"
     else
 
         echo "ERROR mode $mode"
