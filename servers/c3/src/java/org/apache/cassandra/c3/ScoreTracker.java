927b957b078842f1e58ec586c3064b8afd7eb4bd
yuanming <1985161994@qq.com>
2024-06-07 23:05:40 +0800

    [c3] debug c3

--- servers/c3/src/java/org/apache/cassandra/c3/ScoreTracker.java
+++ servers/c3/src/java/org/apache/cassandra/c3/ScoreTracker.java
@@ -38,7 +38,7 @@ public class ScoreTracker
         serviceTimeEMA = getEMA(serviceTime, serviceTimeEMA);
         responseTimeEMA = getEMA(responseTime, responseTimeEMA);
 
-        assert serviceTime <= latency;
+        // assert serviceTime <= latency;
     }
 
     private synchronized double getEMA(double value, double previousEMA)
