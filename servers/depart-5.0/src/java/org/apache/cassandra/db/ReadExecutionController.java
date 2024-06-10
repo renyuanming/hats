f8c80cb012c6666caaca672a1b44bd9955748037
yuanming <1985161994@qq.com>
2024-06-11 00:00:17 +0800

    [depart-5.0] debug getColumnFamilyStorefromMultiReplicas

--- servers/depart-5.0/src/java/org/apache/cassandra/db/ReadExecutionController.java
+++ servers/depart-5.0/src/java/org/apache/cassandra/db/ReadExecutionController.java
@@ -129,7 +129,15 @@ public class ReadExecutionController implements AutoCloseable
     static ReadExecutionController forCommand(ReadCommand command, boolean trackRepairedStatus)
     {
         // ColumnFamilyStore baseCfs = Keyspace.openAndGetStore(command.metadata());
-        ColumnFamilyStore baseCfs = command.getColumnFamilyStorefromMultiReplicas(command.metadata());
+        ColumnFamilyStore baseCfs = null;
+        if(command.metadata().name.contains("usertable"))
+        {
+            baseCfs = command.getColumnFamilyStorefromMultiReplicas(command.metadata());
+        }
+        else
+        {
+            baseCfs = Keyspace.openAndGetStore(command.metadata());
+        }
         ColumnFamilyStore indexCfs = maybeGetIndexCfs(command);
 
         long createdAtNanos = baseCfs.metric.topLocalReadQueryTime.isEnabled() ? clock.now() : NO_SAMPLING;
