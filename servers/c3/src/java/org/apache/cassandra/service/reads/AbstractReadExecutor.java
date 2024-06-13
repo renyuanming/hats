28121946346c76d5e2a1b148f0f07da83a5a6a07
yuanming <1985161994@qq.com>
2024-06-13 16:11:21 +0800

    [c3] debug c3 in abstractReadExecutor

--- servers/c3/src/java/org/apache/cassandra/service/reads/AbstractReadExecutor.java
+++ servers/c3/src/java/org/apache/cassandra/service/reads/AbstractReadExecutor.java
@@ -156,7 +156,7 @@ public abstract class AbstractReadExecutor
 
             for(InetAddressAndPort endpoint : sendRequestAddresses) 
             {
-                if(endpoint.equals(FBUtilities.getBroadcastAddressAndPort()))
+                if(endpoint.getAddress().equals(StorageService.instance.localIP))
                 {
                     hasLocalEndpoint = true;
                     continue;
@@ -216,7 +216,7 @@ public abstract class AbstractReadExecutor
         EndpointsForToken selected = replicaPlan().contacts();
         EndpointsForToken fullDataRequests = selected.filter(Replica::isFull, initialDataRequestCount);
         makeFullDataRequests(fullDataRequests);
-        makeTransientDataRequests(selected.filterLazily(Replica::isTransient));
+        // makeTransientDataRequests(selected.filterLazily(Replica::isTransient));
         makeDigestRequests(selected.filterLazily(r -> r.isFull() && !fullDataRequests.contains(r)));
 
     }
