3e3bd95006869cd3f6e84292d12ba417f5cb9a47
yuanming <1985161994@qq.com>
2024-06-30 22:55:50 +0800

    [scripts] update client

--- scripts/run/start-client.sh
+++ scripts/run/start-client.sh
@@ -41,9 +41,6 @@ func() {
         bin/cqlsh "$coordinator" -e "create keyspace ycsb WITH REPLICATION = {'class' : 'SimpleStrategy', 'replication_factor': $replication_factor };
         USE ycsb;
         create table usertable0 (y_id varchar primary key, field0 varchar);
-        ALTER TABLE usertable0 WITH compaction = { 'class': 'LeveledCompactionStrategy', 'sstable_size_in_mb': ${sstable_size}, 'fanout_size': ${fanout_size}};
-        ALTER TABLE usertable1 WITH compaction = { 'class': 'LeveledCompactionStrategy', 'sstable_size_in_mb': ${sstable_size}, 'fanout_size': ${fanout_size}};
-        ALTER TABLE usertable2 WITH compaction = { 'class': 'LeveledCompactionStrategy', 'sstable_size_in_mb': ${sstable_size}, 'fanout_size': ${fanout_size}};
         consistency all;"
 
         if [ "$compaction_strategy" == "LCS" ]; then
