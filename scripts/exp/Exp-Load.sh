17dbc3d082a3dc879dbd77c775344c65f8b0d3ea
yuanming <1985161994@qq.com>
2024-07-16 20:32:47 +0800

    [scripts] update settings

--- scripts/exp/Exp-Load.sh
+++ scripts/exp/Exp-Load.sh
@@ -3,18 +3,18 @@
 
 . /etc/profile
 
-SCHEMES=("depart-5.0")
+SCHEMES=("mlsm" "depart-5.0")
 REPLICAS=(3)
 SSTABLE_SIZE_IN_MB=160
-KV_NUMBER=30000000
+KV_NUMBER=100000000
 FIELD_LENGTH=1000
 KEY_LENGTH=24
-REBUILD_SERVER="false"
-WAIT_TIME=600
-COMPACTION_STRATEGY=("LCS" "STCS")
+REBUILD_SERVER="true"
+WAIT_TIME=7200
+COMPACTION_STRATEGY=("LCS")
 
 JDK_VERSION="17"
-LOG_LEVEL="info"
+LOG_LEVEL="error"
 BRANCH="main"
 
 
@@ -46,7 +46,7 @@ function main {
         for rf in "${REPLICAS[@]}"; do
             for compaction_strategy in "${COMPACTION_STRATEGY[@]}"; do
                 # Load data
-                load $scheme 64 "${SSTABLE_SIZE_IN_MB}" 2048 "${rf}" "workload_template" ${KV_NUMBER} ${FIELD_LENGTH} ${KEY_LENGTH} ${compaction_strategy} ${LOG_LEVEL} ${BRANCH}
+                load $scheme 4 "${SSTABLE_SIZE_IN_MB}" 2048 "${rf}" "workload_template" ${KV_NUMBER} ${FIELD_LENGTH} ${KEY_LENGTH} ${compaction_strategy} ${LOG_LEVEL} ${BRANCH}
                 # Wait for flush or compaction ready
                 flush "LoadDB" $scheme $WAIT_TIME
                 # Backup the DB and the logs
---
+++ scripts/exp/Exp-Scan.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+. /etc/profile
+
+# Workload Settings
+EXP_NAME="Exp-PureRead"
+WORKLOADS=("workloadScan")
+REQUEST_DISTRIBUTIONS=("zipfian") # zipfian uniform
+OPERATION_NUMBER=2000000
+KV_NUMBER=100000000
+FIELD_LENGTH=1000
+KEY_LENGTH=24
+KEY_LENGTHMin=24
+KEY_LENGTHMax=24
+REPLICAS=(3)
+THREAD_NUMBER=(40)
+MEMTABLE_SIZE=(2048)
+MOTIVATION=("false") # true is only forward the read request to the primary lsm-tree
+MEMORY_LIMIT="12G"
+USE_DIRECTIO=("false") # enable direct io for read path or not
+PURPOSE="ReadAmplification" # To prove ReadAmplification, CompactionOverhead, we select different dataset
+READ_SENSISTIVITY=0.9
+BRANCH="main"
+LOG_LEVEL="error"
+SSTABLE_SIZE_IN_MB=160
+COMPACTION_STRATEGY=("LCS")
+CONSISTENCY_LEVEL=("ONE" "TWO" "ALL")
+
+
+# Debug Settings
+REBUILD_SERVER="false"
+REBUILD_CLIENT="false"
+
+# Server settings
+ROUND_NUMBER=1
+COMPACTION_LEVEL=("all") # zero one all
+
+# Horse
+SCHEDULING_INITIAL_DELAY=600 # seconds
+SCHEDULING_INTERVAL=(60) # seconds
+STATES_UPDATE_INTERVAL=10 # seconds
+THROTLLE_DATA_RATE=(90) # MB/s
+
+JDK_VERSION="17"
+
+SCHEMES=("horse" "depart-5.0")
+
+
+function exportEnv {
+
+    scheme=$1
+
+    export BACKUP_MODE="local"
+    export SCHEME=$scheme # horse or depart
+    export CLUSTER_NAME="1x"
+    SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+    source "${SCRIPT_DIR}/../common.sh"
+    initConf
+}
+
+for scheme in "${SCHEMES[@]}"; do
+    for compaction_strategy in "${COMPACTION_STRATEGY[@]}"; do
+        exportEnv $scheme
+        loadDataset "${EXP_NAME}" "$scheme" "${KV_NUMBER}" "${KEY_LENGTH}" "${FIELD_LENGTH}" "3" "${SSTABLE_SIZE_IN_MB}" "${compaction_strategy}"
+        runExp "${EXP_NAME}" "$scheme" WORKLOADS[@] REQUEST_DISTRIBUTIONS[@] REPLICAS[@] THREAD_NUMBER[@] MEMTABLE_SIZE[@] "${OPERATION_NUMBER}" "${KV_NUMBER}" "${FIELD_LENGTH}" "${KEY_LENGTH}" "${KEY_LENGTHMin}" "${KEY_LENGTHMax}" "${ROUND_NUMBER}" COMPACTION_LEVEL[@]  MOTIVATION[@] "${MEMORY_LIMIT}" USE_DIRECTIO[@] "${REBUILD_SERVER}" "${REBUILD_CLIENT}" "${LOG_LEVEL}" "${BRANCH}" "${PURPOSE}" "${SCHEDULING_INITIAL_DELAY}" SCHEDULING_INTERVAL[@] "${STATES_UPDATE_INTERVAL}" "${READ_SENSISTIVITY}" THROTLLE_DATA_RATE[@] "${JDK_VERSION}" "${SSTABLE_SIZE_IN_MB}" "${compaction_strategy}" CONSISTENCY_LEVEL[@]
+        # cleanup $scheme
+    done
+done
