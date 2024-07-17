a48bb3e221fb9a5fb9a4c68aeee81bf6facbd991
yuanming <1985161994@qq.com>
2024-07-18 00:00:24 +0800

    [scripts] update settings, remove node14 and node17

--- scripts/settings.sh
+++ scripts/settings.sh
@@ -10,8 +10,8 @@ if [[ "$CLUSTER_NAME" == "" ]]; then
     echo "Please specify the CLUSTER_NAME"
     exit 1
 elif [[ "$CLUSTER_NAME" == "1x" ]]; then
-    Servers=("node11" "node12" "node13" "node14" "node15" "node16" "node17" "node18" "node19" "node20")
-    ServersIP=("192.168.10.21" "192.168.10.22" "192.168.10.23" "192.168.10.24" "192.168.10.25" "192.168.10.26" "192.168.10.27" "192.168.10.28" "192.168.10.29" "192.168.10.30")
+    Servers=("node11" "node12" "node13"  "node15" "node16" "node18" "node19" "node20")
+    ServersIP=("192.168.10.21" "192.168.10.22" "192.168.10.23"  "192.168.10.25" "192.168.10.26" "192.168.10.28" "192.168.10.29" "192.168.10.30")
     # Clients=("node17" "node20")
     # ClientsIP=("192.168.10.27" "192.168.10.30")
     Clients=("proj19")
