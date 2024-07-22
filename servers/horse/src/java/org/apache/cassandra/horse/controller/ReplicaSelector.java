0085cffb9a71c392ef5250ca8566b2bc4ce641dd
yuanming <1985161994@qq.com>
2024-07-23 01:04:52 +0800

    [horse] debug sortedByProximityWithBadness

--- servers/horse/src/java/org/apache/cassandra/horse/controller/ReplicaSelector.java
+++ servers/horse/src/java/org/apache/cassandra/horse/controller/ReplicaSelector.java
@@ -148,7 +148,7 @@ public class ReplicaSelector
         double greedyScore = calculateGreedyScore(replicationGroup, targetAddr);
         double latencyScore = calculateLatencyScore(targetAddr);
     
-        return latencyScore + greedyScore;
+        return greedyScore;
     }
     
     private static double calculateGreedyScore(InetAddressAndPort replicationGroup, InetAddressAndPort targetAddr) 
@@ -176,8 +176,8 @@ public class ReplicaSelector
         double latencyScore = 0.0;
         if(snitchMetrics.sampleLatency.containsKey(targetAddr))
         {
-            latencyScore = snitchMetrics.minLatency / snitchMetrics.sampleLatency.get(targetAddr);
-            // latencyScore = snitchMetrics.maxLatency / snitchMetrics.sampleLatency.get(targetAddr);
+            // latencyScore = snitchMetrics.minLatency / snitchMetrics.sampleLatency.get(targetAddr);
+            latencyScore = snitchMetrics.maxLatency / snitchMetrics.sampleLatency.get(targetAddr);
             // latencyScore = snitchMetrics.sampleLatency.get(targetAddr) / snitchMetrics.maxLatency;
             // if (latencyScore >= 1) {
             //     logger.info(ANSI_RED + "rymInfo: the latency score of {} is {}, min is {}, latency is {} " + ANSI_RESET, targetAddr, latencyScore, snitchMetrics.minLatency, snitchMetrics.sampleLatency.get(targetAddr));
@@ -187,7 +187,7 @@ public class ReplicaSelector
         // latencyScore = Math.pow(latencyScore, 3);
         // latencyScore = 1 / (1 + Math.exp(-latencyScore));
 
-        // latencyScore = 1 - Math.exp(-latencyScore);
+        latencyScore = 1 - Math.exp(-latencyScore);
         return latencyScore;
     }
     
