a5f57d9d9ac69ce9e69eeb25f24caeb487818647
renyuanming <1985161994@qq.com>
2024-07-22 16:52:25 +0800

    [horse] debug the compareEndpoints

--- servers/horse/src/java/org/apache/cassandra/locator/DynamicEndpointSnitch.java
+++ servers/horse/src/java/org/apache/cassandra/locator/DynamicEndpointSnitch.java
@@ -280,10 +280,10 @@ public class DynamicEndpointSnitch extends AbstractEndpointSnitch implements Lat
 
         if(scored1.equals(scored2))
             return 0;
-        if(scored1 < scored2)
-            return 1;
-        else
+        if(scored1 > scored2)
             return -1;
+        else
+            return 1;
     }
 
     public int compareEndpoints(InetAddressAndPort target, Replica a1, Replica a2)
