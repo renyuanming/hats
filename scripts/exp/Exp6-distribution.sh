f7dd042bd83be23b0898545bf5233fbfc47f050b
renyuanming <1985161994@qq.com>
2024-08-07 14:42:32 +0800

    [scripts] update the complete scripts for evaluation

--- scripts/exp/Exp6-distribution.sh
+++ scripts/exp/Exp6-distribution.sh
@@ -4,10 +4,10 @@
 . /etc/profile
 
 # Workload Settings
-EXP_NAME="Exp1-ycsb"
+EXP_NAME="Exp6-distribution"
 PURE_READ_WORKLOADS=("workloadc")
-MIXED_READ_WRITE_WORKLOADS=("workloada" "workloadb" "workloadd" "workloade" "workloadf")
-REQUEST_DISTRIBUTIONS=("zipfian") # zipfian uniform
+MIXED_READ_WRITE_WORKLOADS=("workloada")
+REQUEST_DISTRIBUTIONS=("uniform" "zipfian") # zipfian uniform
 OPERATION_NUMBER=25000000
 KV_NUMBER=100000000
 FIELD_LENGTH=(1000)
@@ -33,7 +33,7 @@ REBUILD_SERVER="false"
 REBUILD_CLIENT="false"
 
 # Server settings
-ROUNDS=1
+ROUNDS=5
 COMPACTION_LEVEL=("all") # zero one all
 
 # Horse
@@ -44,7 +44,7 @@ THROTLLE_DATA_RATE=(90) # MB/s
 
 JDK_VERSION="17"
 
-SCHEMES=("horse" "depart-5.0" "c3" "mlsm")
+SCHEMES=("depart-5.0" "c3" "mlsm")
 
 function exportEnv {
     
@@ -61,9 +61,9 @@ function exportEnv {
 for ROUND_NUMBER in $(seq 1 $ROUNDS); do
     for WORKLOAD in "${PURE_READ_WORKLOADS[@]}"; do
         for scheme in "${SCHEMES[@]}"; do
-            exportEnv "${scheme}"
-            
-            runPureReadExp "${EXP_NAME}" "${scheme}" "${WORKLOAD}" REQUEST_DISTRIBUTIONS[@] REPLICAS[@] THREAD_NUMBER[@] MEMTABLE_SIZE[@] "${OPERATION_NUMBER}" "${KV_NUMBER}" FIELD_LENGTH[@] KEY_LENGTH[@] "${KEY_LENGTHMin}" "${KEY_LENGTHMax}" "${ROUND_NUMBER}" COMPACTION_LEVEL[@]  MOTIVATION[@] "${MEMORY_LIMIT}" USE_DIRECTIO[@] "${REBUILD_SERVER}" "${REBUILD_CLIENT}" "${LOG_LEVEL}" "${BRANCH}" "${PURPOSE}" "${SCHEDULING_INITIAL_DELAY}" SCHEDULING_INTERVAL[@] "${STATES_UPDATE_INTERVAL}" "${READ_SENSISTIVITY}" THROTLLE_DATA_RATE[@] "${JDK_VERSION}" "${SSTABLE_SIZE_IN_MB}" COMPACTION_STRATEGY[@] CONSISTENCY_LEVEL[@]
+            # exportEnv "${scheme}"
+            echo "pass"
+            # runPureReadExp "${EXP_NAME}" "${scheme}" "${WORKLOAD}" REQUEST_DISTRIBUTIONS[@] REPLICAS[@] THREAD_NUMBER[@] MEMTABLE_SIZE[@] "${OPERATION_NUMBER}" "${KV_NUMBER}" FIELD_LENGTH[@] KEY_LENGTH[@] "${KEY_LENGTHMin}" "${KEY_LENGTHMax}" "${ROUND_NUMBER}" COMPACTION_LEVEL[@]  MOTIVATION[@] "${MEMORY_LIMIT}" USE_DIRECTIO[@] "${REBUILD_SERVER}" "${REBUILD_CLIENT}" "${LOG_LEVEL}" "${BRANCH}" "${PURPOSE}" "${SCHEDULING_INITIAL_DELAY}" SCHEDULING_INTERVAL[@] "${STATES_UPDATE_INTERVAL}" "${READ_SENSISTIVITY}" THROTLLE_DATA_RATE[@] "${JDK_VERSION}" "${SSTABLE_SIZE_IN_MB}" COMPACTION_STRATEGY[@] CONSISTENCY_LEVEL[@]
 
         done
     done
