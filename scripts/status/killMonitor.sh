cd2774e8e3d14264cc4568184c8d57b449c96ef6
yuanming <1985161994@qq.com>
2024-08-17 19:42:30 +0800

    [script] update for measuring the memory

--- scripts/status/killMonitor.sh
+++ scripts/status/killMonitor.sh
@@ -2,13 +2,13 @@
 . /etc/profile
 
 while true; do
-    kill -9 $(ps aux | grep "statsCPU_RAM.sh" | grep -v grep | awk 'NR == 1' | awk '{print $2}')
+    kill -9 $(ps aux | grep "statsRAM.sh" | grep -v grep | awk 'NR == 1' | awk '{print $2}')
     sleep 1 # You can adjust the sleep duration as needed.
     if [ $? -eq 0 ]; then
-        echo "Kill statsCPU_RAM.sh successfully."
+        echo "Kill statsRAM.sh successfully."
         exit 0
     else
-        echo "Not found statsCPU_RAM.sh."
+        echo "Not found statsRAM.sh."
         exit 1
     fi
 done
---
+++ scripts/status/statsRAM.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+. /etc/profile
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+source "${SCRIPT_DIR}/../settings.sh"
+
+function captureCPU_RAM {
+    expName=$1
+    stage=$2
+    resultDir=$3
+    SimpleBase=5
+
+    CASSANDRA_PID=$(ps aux | grep CassandraDaemon | grep -v grep | awk '{print $2}')
+    echo "Cassandra PID: $CASSANDRA_PID"
+
+    OUTPUT_MEM="${resultDir}/${expName}_${stage}_memory_usage.txt"
+    echo "Memory overhead for Cassandra PID: $CASSANDRA_PID, Exp = ${expName}, stage = $stage" >>"$OUTPUT_MEM"
+
+    while true; do
+        # If Cassandra is running, get its memory usage
+        if [[ ! -z "$CASSANDRA_PID" ]]; then
+            MEM_USAGE=$(ps -o rss= -p "$CASSANDRA_PID")
+            echo "$(date): Cassandra PID $CASSANDRA_PID is using $MEM_USAGE KiB" >>"$OUTPUT_MEM"
+        else
+            echo "$(date): Cassandra is not running" >>"$OUTPUT_MEM"
+            exit
+        fi
+        # Wait for $SimpleBase seconds before the next check
+        sleep $SimpleBase
+    done
+}
+
+captureCPU_RAM "$1" "$2"
