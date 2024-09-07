abed9e58b75c2969f0e07bdfeeff447c66561ce2
yuanming <1985161994@qq.com>
2024-09-07 19:21:05 +0800

    [horse] rename the load balancer

--- servers/horse/src/java/org/apache/cassandra/horse/controller/LoadBalancer.java
+++ servers/horse/src/java/org/apache/cassandra/horse/controller/LoadBalancer.java
@@ -22,7 +22,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class PseudoCode {
+public class LoadBalancer {
 
     public static double[][] balanceLoad(int N, int R, int W, double[] L, double[] T, double[][] C, double[] lambda) {
 
