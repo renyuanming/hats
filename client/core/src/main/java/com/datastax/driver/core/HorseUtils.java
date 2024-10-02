fcc2988377b819bf8110b319f2c14837cd7d63cc
renyuanming <1985161994@qq.com>
2024-10-02 13:33:29 +0800

    [client] add logs

--- client/core/src/main/java/com/datastax/driver/core/HorseUtils.java
+++ client/core/src/main/java/com/datastax/driver/core/HorseUtils.java
@@ -94,10 +94,18 @@ public class HorseUtils
             {
                 averageReadLatency = (entry.getValue().getCount() * 1.0 / totalReadCount) * this.readLatency.get(entry.getKey());
                 averageCoordinatorLatency = (entry.getValue().getCount() * 1.0 / totalReadCount) * this.coordinatorReadLatency.get(entry.getKey());
+            } 
+            // calculate the cov of the read latency
+            double sum = 0.0;
+            for (Map.Entry<InetAddress, HorseLatencyTracker> entry : readLatencyTrackers.entrySet())
+            {
+                sum += Math.pow(this.coordinatorReadLatency.get(entry.getKey()) - averageCoordinatorLatency, 2);
             }
+            double cov = Math.sqrt(sum / totalReadCount) / averageCoordinatorLatency;
 
             // TODO recalculate the coordinator weight for each replication group
             this.coordinatorWeight = averageCoordinatorLatency / averageReadLatency;
+            logger.info("rymInfo: the average coordinator read latency across all nodes is {}, the cov is {}", averageCoordinatorLatency, cov);
 
         }
     }
