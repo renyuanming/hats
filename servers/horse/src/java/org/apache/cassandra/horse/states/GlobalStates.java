6d47346edd16bcbe7d1a46454eccbe1c83c1016d
yuanming <1985161994@qq.com>
2024-12-04 22:59:13 +0800

    [HATS] debug

--- servers/horse/src/java/org/apache/cassandra/horse/states/GlobalStates.java
+++ servers/horse/src/java/org/apache/cassandra/horse/states/GlobalStates.java
@@ -161,7 +161,7 @@ public class GlobalStates implements Serializable {
             readCountOfEachReplica[i] = (int) (GlobalStates.globalStates.readCountOfEachRG[rgIndex] * GlobalStates.globalPolicy[nodeIndex][i]);
             totalReadCountOfTheNode += readCountOfEachReplica[i];
         }
-        logger.debug("rymDebug: The read count of each replica is {}, the total read count of the node is {}", Arrays.toString(readCountOfEachReplica), totalReadCountOfTheNode);
+        logger.info("rymInfo: The read count of each replica is {}, the total read count of the node is {}", Arrays.toString(readCountOfEachReplica), totalReadCountOfTheNode);
         GlobalStates.expectedRequestNumber[nodeIndex] = totalReadCountOfTheNode;
         Double[] localPolicyForBackgroundController = new Double[rf];
         for(int i = 0; i < rf; i++)
