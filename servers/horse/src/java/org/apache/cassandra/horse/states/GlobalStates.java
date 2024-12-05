4f287824ab6b82b10bc9a09f37117a69356bd363
yuanming <1985161994@qq.com>
2024-12-05 21:40:51 +0800

    [HATS] debug

--- servers/horse/src/java/org/apache/cassandra/horse/states/GlobalStates.java
+++ servers/horse/src/java/org/apache/cassandra/horse/states/GlobalStates.java
@@ -169,6 +169,7 @@ public class GlobalStates implements Serializable {
         int nodeCount = Gossiper.getAllHosts().size();
         logger.info("rymInfo: Updating the policy for current node, the node count is {}", nodeCount);
         // update expectedRequestNumberOfEachRG
+        GlobalStates.expectedRequestNumberOfEachRG = new int[nodeCount];
         for (int i= 0; i < nodeCount; i++)
         {
             int requestCount = 0;
@@ -178,10 +179,12 @@ public class GlobalStates implements Serializable {
                 logger.info("rymInfo: the expected request distribution is {}, rgIndex is {}, replica index is {}, curNodeIndex is {}", GlobalStates.expectedStates.expectedRequestDistribution[curNodeIndex], i, j, curNodeIndex);
                 requestCount += GlobalStates.expectedStates.expectedRequestDistribution[curNodeIndex][j];
             }
+            logger.info("rymInfo: the expected request number of each RG {} is {}", i, requestCount);
             GlobalStates.expectedRequestNumberOfEachRG[i] = requestCount;
         }
         logger.info("rymInfo: the expected request number of each RG is {}", GlobalStates.expectedRequestNumberOfEachRG);
         
+        GlobalStates.expectedRequestNumberofEachNode = new int[nodeCount];
         // Update expectedRequestNumberofEachNode
         for(int i = 0; i < nodeCount; i++)
         {
