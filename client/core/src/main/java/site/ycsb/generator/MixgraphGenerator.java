563f05887db89d032050d4299b45bdf7e9c6d050
renyuanming <renyuanming1@gmail.com>
2025-04-30 14:48:55 +0800

    update client

--- client/core/src/main/java/site/ycsb/generator/MixgraphGenerator.java
+++ client/core/src/main/java/site/ycsb/generator/MixgraphGenerator.java
@@ -58,7 +58,7 @@ public class MixgraphGenerator extends NumberGenerator {
   private static final double KEYRANGE_DIST_B = -2.917;
   private static final double KEYRANGE_DIST_C = 0.0164;
   private static final double KEYRANGE_DIST_D = -0.08082;
-  private static int KEYRANGE_NUM = 10000000;
+  private static int KEYRANGE_NUM = 1000000;
   private static long KEYRANGE_SIZE = 0;
   private static long KEYRANGE_RAND_MAX = 0;
   private List<KeyRangeUnit> keyrange_set_ = new ArrayList<>();
